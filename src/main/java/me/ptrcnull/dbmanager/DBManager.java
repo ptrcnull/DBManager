340977e9e1bc52ff839ea0406b2ba1949c80beee
ptrcnull <git@ptrcnull.me>
2020-03-17 14:20:04 +0100

    feat: Added query and update methods

--- src/main/java/me/ptrcnull/dbmanager/DBManager.java
+++ src/main/java/me/ptrcnull/dbmanager/DBManager.java
@@ -3,7 +3,11 @@ package me.ptrcnull.dbmanager;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
-import java.sql.*;
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public abstract class DBManager {
     private HikariConfig hikariConfig;
@@ -39,9 +43,53 @@ public abstract class DBManager {
         return dataSource.getConnection();
     }
 
-//    public PreparedStatement prepare(String statement) throws SQLException {
-//        return getConnection().prepareStatement(statement);
-//    }
+    private PreparedStatement prepare(Connection conn, String sql, Object... args) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement(sql);
+        for (int i = 1; i <= args.length; i++) {
+            Object arg = args[i];
+            if (arg instanceof String) {
+                stmt.setString(i, (String) arg);
+            } else if (arg instanceof Integer) {
+                stmt.setInt(i, (Integer) arg);
+            } else if (arg instanceof Boolean) {
+                stmt.setBoolean(i, (Boolean) arg);
+            } else if (arg instanceof BigDecimal) {
+                stmt.setBigDecimal(i, (BigDecimal) arg);
+            } else if (arg instanceof Double) {
+                stmt.setDouble(i, (Double) arg);
+            } else if (arg instanceof Float) {
+                stmt.setFloat(i, (Float) arg);
+            } else if (arg instanceof Long) {
+                stmt.setLong(i, (Long) arg);
+            } else if (arg instanceof Short) {
+                stmt.setShort(i, (Short) arg);
+            } else {
+                stmt.setObject(i, arg);
+            }
+        }
+        return stmt;
+    }
+
+    public ResultSet query(String sql, Object... args) throws SQLException {
+        Connection conn = getConnection();
+        PreparedStatement stmt = prepare(conn, sql, args);
+
+        ResultSet result = stmt.executeQuery();
+
+        stmt.close();
+        conn.close();
+        return result;
+    }
+
+    public void update(String sql, Object... args) throws SQLException {
+        Connection conn = getConnection();
+        PreparedStatement stmt = prepare(conn, sql, args);
+
+        stmt.executeUpdate();
+
+        stmt.close();
+        conn.close();
+    }
 
     public boolean setup() {
         dataSource = new HikariDataSource(hikariConfig);
